b6659b22ec945776696c9df0a7a134dbd4cd3132
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-07-15 13:23:31 +0200

    Add TextButton

---
+++ ButtonTest/Assets/Scripts/TextButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class TextButton : MonoBehaviour
+{
+
+    public Text m_textPrefab;
+    public Transform m_textShowArea;
+    public float m_borderOffsetHorizontal;
+    public float m_borderOffsetVertical;
+
+    private Vector3 GetRandomPositionInShowArea(){
+        Vector3[] v = new Vector3[4];
+        m_textShowArea.GetComponent<RectTransform>().GetLocalCorners(v);
+        return new Vector3( Random.Range(v[0].x + m_borderOffsetHorizontal, v[2].x - m_borderOffsetHorizontal), 
+                            Random.Range(v[0].y + m_borderOffsetVertical,   v[2].y - m_borderOffsetVertical), 0 );
+    }
+
+    public void OnButtonPress(){
+        Text tempTextBox = Instantiate(m_textPrefab, GetRandomPositionInShowArea(), m_textShowArea.rotation );
+        tempTextBox.transform.SetParent(m_textShowArea, false);
+    }
+
+}
---
+++ ButtonTest/Assets/Scripts/TextTimeLife.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class TextTimeLife : MonoBehaviour
+{
+    public float secondsToDestroy = 0;
+    private float timeOfExistence = 0;
+
+    private Animator m_animator;
+
+    void Start(){
+        m_animator = GetComponent<Animator>();
+        Destroy( gameObject, secondsToDestroy );
+    }
+
+    void Update()
+    {
+        timeOfExistence += Time.deltaTime;
+        m_animator.SetFloat("timeOnScene", timeOfExistence);
+        GetComponent<Text>().text = "This text will deaspear in " +  Mathf.Max( 3.2f - timeOfExistence, 0 ).ToString();
+    }
+}
