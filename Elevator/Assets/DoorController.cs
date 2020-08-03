df62f18fe8d7d7e3814481df0a29bc48d703c39b
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-08-03 02:56:23 +0200

    Update

---
+++ Elevator/Assets/DoorController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    private Animator m_animator;
+
+    private bool hodor = false;
+
+    private float timeToCloseTheDoor = 0;
+
+    [SerializeField] private float amountOfTimeRequiredToShutDoor = 10f; 
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        timeToCloseTheDoor = amountOfTimeRequiredToShutDoor;
+    }
+
+    void Update()
+    {
+        UpdateCloseTimer();
+        UpdateAnimator();
+    }
+
+    void UpdateAnimator(){
+        m_animator.SetFloat("timeToClose", timeToCloseTheDoor);
+    }
+
+    void UpdateCloseTimer(){
+        timeToCloseTheDoor -= Time.deltaTime;
+        if( hodor ) timeToCloseTheDoor = amountOfTimeRequiredToShutDoor;
+    }
+
+    public void OpenTheDoor(){
+        timeToCloseTheDoor = amountOfTimeRequiredToShutDoor;
+    }
+
+    public void CloseTheDoor(){
+        timeToCloseTheDoor = -1;
+    }
+
+    public bool AreDoorsClosed(){
+        return m_animator.GetCurrentAnimatorStateInfo(0).IsName("DoorClosed");
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        if( other.tag.Contains("MainCamera") ){
+            hodor = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if( other.tag.Contains("MainCamera") ){
+            hodor = false;
+        }
+    }
+}
---
+++ Elevator/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;
+    public float moveSpeed = 2.0f;
+    
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 90.0f;
+    private float rotX;
+    
+    public GameObject dot;
+
+
+    void Update ()
+    {
+        MouseAiming();
+        KeyboardMovement();
+    
+        HandleClick();
+    }
+    
+    void HandleClick(){
+        if( !Input.GetButtonDown("Fire1") ) return;
+    
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100.0f))
+ 
+        {
+            if( hit.transform != null ){
+                
+                IPressButton buttonToPress = hit.transform.GetComponent<IPressButton>();
+                if( buttonToPress != null ){
+                    buttonToPress.PressButton();
+                }
+            }
+        }
+    }
+
+
+
+    void MouseAiming ()
+    {
+        float y = Input.GetAxis("Mouse X") * turnSpeed;
+        rotX   += Input.GetAxis("Mouse Y") * turnSpeed;
+    
+        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+    
+        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+    }
+    
+    void KeyboardMovement ()
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+    
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+    
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+    }
+}
